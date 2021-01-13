4d0684523fdb2037ee5cc83633695b2ee4fd4ae3
tlondero <tlondero@itba.edu.ar>
2021-01-13 00:24:09 -0300

    ._.

--- TestS/DAC_Test/dacdmatest_dac_continuous_pdb_edma/source/dac_continuous_pdb_edma.c
+++ TestS/DAC_Test/dacdmatest_dac_continuous_pdb_edma/source/dac_continuous_pdb_edma.c
@@ -19,53 +19,31 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-//Bus_clock.outFreq, value: 60 MHz
-
-#define DEMO_PDB_BASEADDR           PDB0
-
-#define DEMO_PDB_MODULUS_VALUE      0xFFFU
-#define AUDIO_PDB_MODULUS_VALUE      0x21U
-
-#define DEMO_PDB_DELAY_VALUE        0U
-#define DEMO_PDB_DAC_CHANNEL        kPDB_DACTriggerChannel0
-
-#define DEMO_PDB_DAC_INTERVAL_VALUE 0xFFFU
-#define AUDIO_PDB_DAC_INTERVAL_VALUE 0x21U
-
-#define DEMO_DAC_BASEADDR           DAC0
-#define DEMO_DMAMUX_BASEADDR        DMAMUX
-#define DEMO_DMA_CHANNEL            0U
-#define DEMO_DMA_DAC_SOURCE         45U
-#define DEMO_DMA_BASEADDR           DMA0
+#define PDB_BASEADDR         		PDB0
+#define PDB_MODULUS_VALUE      		0x21U
+#define PDB_DELAY_VALUE        		0U
+#define PDB_DAC_CHANNEL        		kPDB_DACTriggerChannel0
+#define PDB_DAC_INTERVAL_VALUE 		0x21U
+#define DAC_BASEADDR           		DAC0
+#define DMAMUX_BASEADDR        		DMAMUX
+#define DMA_CHANNEL            		0U
+#define DMA_DAC_SOURCE         		45U
+#define DMA_BASEADDR           		DMA0
 #define DAC_DATA_REG_ADDR           0x400cc000U
-#define DEMO_DMA_IRQ_ID             DMA0_IRQn
-#define DEMO_DAC_USED_BUFFER_SIZE 32U
+#define DMA_IRQ_ID             		DMA0_IRQn
+#define DAC_USED_BUFFER_SIZE 		32U
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
-/*!
- * @brief Initialize the EDMA.
- */
+
 static void EDMA_Configuration(void);
-/*!
- * @brief Initialize the DMAMUX.
- */
+
 static void DMAMUX_Configuration(void);
-/*!
- * @brief Initialize the PIT.
- */
+
 static void PDB_Configuration(void);
-/*!
- * @brief Initialize the PDB.
- */
-//static void PDB_Configuration(void);
-/*!
- * @brief Initialize the DAC.
- */
+
 static void DAC_Configuration(void);
-/*!
- * @brief Callback function for EDMA.
- */
+
 static void Edma_Callback(edma_handle_t *handle, void *userData, bool transferDone, uint32_t tcds);
 /*******************************************************************************
  * Variables
@@ -73,7 +51,7 @@ static void Edma_Callback(edma_handle_t *handle, void *userData, bool transferDo
 edma_handle_t g_EDMA_Handle;                             /* Edma handler */
 edma_transfer_config_t g_transferConfig;                 /* Edma transfer config. */
 volatile uint32_t g_index                          = 0U; /* Index of the g_dacDataArray array. */
-uint16_t g_dacDataArray[DEMO_DAC_USED_BUFFER_SIZE] = {
+uint16_t g_dacDataArray[DAC_USED_BUFFER_SIZE] = {
     0U,    401U,  799U,  1188U, 1567U, 1930U, 2275U, 2598U, 2895U, 3165U, 3405U, 3611U, 3783U, 3918U, 4016U, 4075U,
     4095U, 4075U, 4016U, 3918U, 3783U, 3611U, 3405U, 3165U, 2895U, 2598U, 2275U, 1930U, 1567U, 1188U, 799U,  401U};
 /*******************************************************************************
@@ -101,7 +79,7 @@ int main(void)
     DAC_Configuration();
 
     /* Generate continuous trigger signal to DAC. */
-    PDB_DoSoftwareTrigger(DEMO_PDB_BASEADDR);
+    PDB_DoSoftwareTrigger(PDB_BASEADDR);
 
     PRINTF("Please probe the DAC output with a oscilloscope.\r\n");
     while (1)
@@ -114,18 +92,18 @@ static void EDMA_Configuration(void)
     edma_config_t userConfig;
 
     EDMA_GetDefaultConfig(&userConfig);
-    EDMA_Init(DEMO_DMA_BASEADDR, &userConfig);
-    EDMA_CreateHandle(&g_EDMA_Handle, DEMO_DMA_BASEADDR, DEMO_DMA_CHANNEL);
+    EDMA_Init(DMA_BASEADDR, &userConfig);
+    EDMA_CreateHandle(&g_EDMA_Handle, DMA_BASEADDR, DMA_CHANNEL);
     EDMA_SetCallback(&g_EDMA_Handle, Edma_Callback, NULL);
     EDMA_PrepareTransfer(&g_transferConfig, (void *)(g_dacDataArray + g_index), sizeof(uint16_t),
                          (void *)DAC_DATA_REG_ADDR, sizeof(uint16_t), DAC_DATL_COUNT * sizeof(uint16_t),
                          DAC_DATL_COUNT * sizeof(uint16_t), kEDMA_MemoryToMemory);
     EDMA_SubmitTransfer(&g_EDMA_Handle, &g_transferConfig);
     /* Enable interrupt when transfer is done. */
-    EDMA_EnableChannelInterrupts(DEMO_DMA_BASEADDR, DEMO_DMA_CHANNEL, kEDMA_MajorInterruptEnable);
+    EDMA_EnableChannelInterrupts(DMA_BASEADDR, DMA_CHANNEL, kEDMA_MajorInterruptEnable);
 #if defined(FSL_FEATURE_EDMA_ASYNCHRO_REQUEST_CHANNEL_COUNT) && FSL_FEATURE_EDMA_ASYNCHRO_REQUEST_CHANNEL_COUNT
     /* Enable async DMA request. */
-    EDMA_EnableAsyncRequest(DEMO_DMA_BASEADDR, DEMO_DMA_CHANNEL, true);
+    EDMA_EnableAsyncRequest(DMA_BASEADDR, DMA_CHANNEL, true);
 #endif /* FSL_FEATURE_EDMA_ASYNCHRO_REQUEST_CHANNEL_COUNT */
     /* Enable transfer. */
     EDMA_StartTransfer(&g_EDMA_Handle);
@@ -134,9 +112,9 @@ static void EDMA_Configuration(void)
 static void DMAMUX_Configuration(void)
 {
     /* Configure DMAMUX */
-    DMAMUX_Init(DEMO_DMAMUX_BASEADDR);
-    DMAMUX_SetSource(DEMO_DMAMUX_BASEADDR, DEMO_DMA_CHANNEL, DEMO_DMA_DAC_SOURCE); /* Map ADC source to channel 0 */
-    DMAMUX_EnableChannel(DEMO_DMAMUX_BASEADDR, DEMO_DMA_CHANNEL);
+    DMAMUX_Init(DMAMUX_BASEADDR);
+    DMAMUX_SetSource(DMAMUX_BASEADDR, DMA_CHANNEL, DMA_DAC_SOURCE); /* Map ADC source to channel 0 */
+    DMAMUX_EnableChannel(DMAMUX_BASEADDR, DMA_CHANNEL);
 }
 
 /* Enable the trigger source of PDB. */
@@ -145,22 +123,23 @@ static void PDB_Configuration(void)
     pdb_config_t pdbConfigStruct;
     pdb_dac_trigger_config_t pdbDacTriggerConfigStruct;
 
+
     PDB_GetDefaultConfig(&pdbConfigStruct);
     pdbConfigStruct.dividerMultiplicationFactor = kPDB_DividerMultiplicationFactor40;
     pdbConfigStruct.enableContinuousMode        = true;
-    PDB_Init(DEMO_PDB_BASEADDR, &pdbConfigStruct);
-    PDB_EnableInterrupts(DEMO_PDB_BASEADDR, kPDB_DelayInterruptEnable);
-    PDB_SetModulusValue(DEMO_PDB_BASEADDR, AUDIO_PDB_MODULUS_VALUE);
-    PDB_SetCounterDelayValue(DEMO_PDB_BASEADDR, DEMO_PDB_DELAY_VALUE);
+    PDB_Init(PDB_BASEADDR, &pdbConfigStruct);
+    PDB_EnableInterrupts(PDB_BASEADDR, kPDB_DelayInterruptEnable);
+    PDB_SetModulusValue(PDB_BASEADDR, PDB_MODULUS_VALUE);
+    PDB_SetCounterDelayValue(PDB_BASEADDR, PDB_DELAY_VALUE);
 
     /* Set DAC trigger. */
     pdbDacTriggerConfigStruct.enableExternalTriggerInput = false;
     pdbDacTriggerConfigStruct.enableIntervalTrigger      = true;
-    PDB_SetDACTriggerConfig(DEMO_PDB_BASEADDR, DEMO_PDB_DAC_CHANNEL, &pdbDacTriggerConfigStruct);
-    PDB_SetDACTriggerIntervalValue(DEMO_PDB_BASEADDR, DEMO_PDB_DAC_CHANNEL, AUDIO_PDB_DAC_INTERVAL_VALUE);
+    PDB_SetDACTriggerConfig(PDB_BASEADDR, PDB_DAC_CHANNEL, &pdbDacTriggerConfigStruct);
+    PDB_SetDACTriggerIntervalValue(PDB_BASEADDR, PDB_DAC_CHANNEL, PDB_DAC_INTERVAL_VALUE);
 
     /* Load PDB values. */
-    PDB_DoLoadValues(DEMO_PDB_BASEADDR);
+    PDB_DoLoadValues(PDB_BASEADDR);
 }
 
 static void DAC_Configuration(void)
@@ -169,35 +148,35 @@ static void DAC_Configuration(void)
     dac_buffer_config_t dacBufferConfigStruct;
 
     DAC_GetDefaultConfig(&dacConfigStruct);
-    DAC_Init(DEMO_DAC_BASEADDR, &dacConfigStruct);
-    DAC_Enable(DEMO_DAC_BASEADDR, true); /* Enable output. */
+    DAC_Init(DAC_BASEADDR, &dacConfigStruct);
+    DAC_Enable(DAC_BASEADDR, true); /* Enable output. */
 
     /* Configure the DAC buffer. */
-    DAC_EnableBuffer(DEMO_DAC_BASEADDR, true);
+    DAC_EnableBuffer(DAC_BASEADDR, true);
     DAC_GetDefaultBufferConfig(&dacBufferConfigStruct);
     dacBufferConfigStruct.triggerMode = kDAC_BufferTriggerByHardwareMode;
-    DAC_SetBufferConfig(DEMO_DAC_BASEADDR, &dacBufferConfigStruct);
-    DAC_SetBufferReadPointer(DEMO_DAC_BASEADDR, 0U); /* Make sure the read pointer to the start. */
+    DAC_SetBufferConfig(DAC_BASEADDR, &dacBufferConfigStruct);
+    DAC_SetBufferReadPointer(DAC_BASEADDR, 0U); /* Make sure the read pointer to the start. */
 
     /* Enable DMA. */
-    DAC_EnableBufferInterrupts(DEMO_DAC_BASEADDR, kDAC_BufferReadPointerTopInterruptEnable);
-    DAC_EnableBufferDMA(DEMO_DAC_BASEADDR, true);
+    DAC_EnableBufferInterrupts(DAC_BASEADDR, kDAC_BufferReadPointerTopInterruptEnable);
+    DAC_EnableBufferDMA(DAC_BASEADDR, true);
 }
 
 static void Edma_Callback(edma_handle_t *handle, void *userData, bool transferDone, uint32_t tcds)
 {
     /* Clear Edma interrupt flag. */
-    EDMA_ClearChannelStatusFlags(DEMO_DMA_BASEADDR, DEMO_DMA_CHANNEL, kEDMA_InterruptFlag);
+    EDMA_ClearChannelStatusFlags(DMA_BASEADDR, DMA_CHANNEL, kEDMA_InterruptFlag);
     /* Setup transfer */
     g_index += DAC_DATL_COUNT;
-    if (g_index == DEMO_DAC_USED_BUFFER_SIZE)
+    if (g_index == DAC_USED_BUFFER_SIZE)
     {
         g_index = 0U;
     }
     EDMA_PrepareTransfer(&g_transferConfig, (void *)(g_dacDataArray + g_index), sizeof(uint16_t),
                          (void *)DAC_DATA_REG_ADDR, sizeof(uint16_t), DAC_DATL_COUNT * sizeof(uint16_t),
                          DAC_DATL_COUNT * sizeof(uint16_t), kEDMA_MemoryToMemory);
-    EDMA_SetTransferConfig(DEMO_DMA_BASEADDR, DEMO_DMA_CHANNEL, &g_transferConfig, NULL);
+    EDMA_SetTransferConfig(DMA_BASEADDR, DMA_CHANNEL, &g_transferConfig, NULL);
     /* Enable transfer. */
     EDMA_StartTransfer(&g_EDMA_Handle);
 }
