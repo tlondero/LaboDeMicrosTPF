a71b0f08b5ddd2beb22c5c57abe6bd5467162442
Alan <31071359+amechu@users.noreply.github.com>
2021-01-23 16:38:52 -0300

    app created

---
+++ App/MP3Player/source/DAC_Wrapper.h
@@ -0,0 +1,78 @@
+/***************************************************************************//**
+ @file     DAC_Wraper.h
+ @brief
+ @author   MAGT
+ ******************************************************************************/
+#ifndef DAC_WRAPER_H_
+#define DAC_WRAPER_H_
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
+#include "fsl_debug_console.h"
+#include "board.h"
+
+#include "fsl_common.h"
+#include "clock_config.h"
+#include "pin_mux.h"
+
+#include "fsl_pdb.h"
+
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+
+#define DAC_USED_BUFFER_SIZE 		10240U
+
+/*******************************************************************************
+ * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
+ ******************************************************************************/
+
+typedef enum {
+	kMP3_8000Hz = 8000,
+	kMP3_11025z = 11025,
+	kMP3_12000Hz = 12000,
+	kMP3_16000Hz = 16000,
+	kMP3_22050Hz = 22050,
+	kMP3_24000Hz = 24000,
+	kMP3_32000Hz = 32000,
+	kMP3_44100Hz = 44100,
+	kMP3_48000Hz = 48000
+} mp3_sample_rate_t;
+
+typedef enum {
+	kMP3_Mono = 1,
+	kMP3_Stereo = 2
+} mp3_audio_mode_t;
+
+/*******************************************************************************
+ * VARIABLE PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+void DAC_Wrapper_Init(void);
+
+void DAC_Wrapper_Set_Data_Array(void *newDataArray, uint32_t newSizeOf);
+
+void DAC_Wrapper_Set_Next_Buffer(void *forcedBackUp);
+
+void DAC_Wrapper_Clear_Data_Array(void);
+
+void DAC_Wrapper_Start_Trigger(void);		//Start trigger signal to DAC
+
+void DAC_Wrapper_Loop(bool status);
+
+void DAC_Wrapper_PDB_Config(uint32_t mod_val,
+		pdb_divider_multiplication_factor_t mult_fact,
+		pdb_prescaler_divider_t prescaler);
+
+bool MP3_Set_Sample_Rate(uint16_t sr, uint8_t ch);
+
+bool DAC_Wrapper_Is_Transfer_Done(void);
+
+void DAC_Wrapper_Clear_Transfer_Done(void);
+
+#endif /* DAC_WRAPER_H_ */
---
+++ App/MP3Player/source/MK64FN1M0xxx12_Project.c
@@ -0,0 +1,207 @@
+/**
+ * @file    MK64FN1M0xxx12_Project.c
+ * @brief   Application entry point.
+ */
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "debug_ifdefs.h"
+#include "MK64F12.h"
+#include "fsl_sysmpu.h"
+#include "fsl_debug_console.h"
+#include "sdmmc_config.h"
+#include "fsl_sd_disk.h"
+#include "fsl_common.h"
+#include "fsl_pit.h"
+#include "SD_Detect_Wraper.h"
+#include "mp3Decoder.h"
+#include "ff.h"
+
+#include "DAC_Wrapper.h"
+
+#define MAX_DAC				4095
+#define MP3_MAX_VALUE		100000//32767
+#define MP3_MIN_VALUE		-100000//-32768
+#define MP3_GAP				MP3_MAX_VALUE - MP3_MIN_VALUE
+
+char* concat(const char *s1, const char *s2);
+
+void cbackin(void) {
+#ifdef DEBUG_PRINTF_APP
+	printf("[App] SD Card inserted.\r\n");
+#endif
+	MP3DecoderInit();
+}
+void cbackout(void) {
+#ifdef DEBUG_PRINTF_APP
+	printf("[App] SD Card removed.\r\n");
+#endif
+}
+
+#ifdef DEBUG_FRAME_DELAY
+bool nextFrameFlag = false;
+#endif
+
+static short buffer_1[MP3_DECODED_BUFFER_SIZE];
+static short buffer_2[MP3_DECODED_BUFFER_SIZE];
+
+mp3_decoder_frame_data_t frameData;
+mp3_decoder_tag_data_t ID3Data;
+
+int main(void) {
+	uint16_t sampleCount;
+#ifdef DEBUG_PRINTF_INFO
+	uint32_t sr = 0;
+#endif
+
+	/* Init board hardware. */
+	BOARD_InitBootPins();
+	BOARD_InitBootClocks();
+#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
+
+	/* Init FSL debug console. */
+	BOARD_InitDebugConsole();
+#endif
+	SDWraperInit(cbackin, cbackout);
+/////////////////////////////////////////////////////////////
+	bool finished = false;
+	while (!finished) {
+		if (getJustIn()) {
+
+			if (MP3LoadFile("test_500.mp3", "test_500.wav")) {
+			//if (MP3LoadFile("dakiti.mp3", "dakiti.wav")) {
+				int i = 0;
+				if (MP3GetTagData(&ID3Data)) {
+					printf("\nSONG INFO\n");
+					printf("TITLE: %s\n", ID3Data.title);
+					printf("ARTIST: %s\n", ID3Data.artist);
+					printf("ALBUM: %s\n", ID3Data.album);
+					printf("TRACK NUM: %s\n", ID3Data.trackNum);
+					printf("YEAR: %s\n", ID3Data.year);
+				}
+
+				DAC_Wrapper_Init();
+				DAC_Wrapper_Loop(false);
+				DAC_Wrapper_Start_Trigger();
+
+				//Empiezo por el buffer 1
+				mp3_decoder_result_t res = MP3GetDecodedFrame(buffer_1,
+				MP3_DECODED_BUFFER_SIZE, &sampleCount, 0);
+				bool using_buffer_1 = true;
+
+				uint16_t sr_ = kMP3_44100Hz;	//Default config 4 mp3 stereo
+				uint8_t ch_ = kMP3_Stereo;
+				MP3_Set_Sample_Rate(sr_, ch_);
+				//Por defecto ya está configurado así, solo lo explicito y ayuda
+				//si hay que cambiarlo mientras corre el codigo
+
+				printf("\nHolu");
+
+				while (true) {
+
+#ifdef DEBUG_PRINTF_INFO
+					printf("\n[APP] Frame %d decoding started.\n", i);
+#endif
+
+					if (res == MP3DECODER_NO_ERROR) {
+
+						if (DAC_Wrapper_Is_Transfer_Done() || i == 0) {	//Entro en la primera o cuando ya transmiti
+
+							MP3GetLastFrameData(&frameData);
+
+							//No debería cambiar el sample rate entre frame y frame
+							//Pero si lo hace...
+							if ((sr_ != frameData.sampleRate)
+									|| (ch_ != frameData.channelCount)) {
+								sr_ = frameData.sampleRate;
+								ch_ = frameData.channelCount;
+								MP3_Set_Sample_Rate(sr_, ch_);
+							}
+
+							DAC_Wrapper_Clear_Transfer_Done();
+
+							//uint16_t* nullptr = NULL;
+							if (using_buffer_1) {
+								//Envio el buffer 1 al dac
+								DAC_Wrapper_Set_Data_Array(&buffer_1,
+										frameData.sampleCount);
+								DAC_Wrapper_Set_Next_Buffer(&buffer_2);
+
+								//Cargo el y normalizo el buffer 2
+								res = MP3GetDecodedFrame(buffer_2,
+								MP3_DECODED_BUFFER_SIZE, &sampleCount, 0);
+								uint16_t j;
+								for (j = 0; j < frameData.sampleCount; j++) {
+									buffer_2[j] = (uint16_t) ((buffer_2[j]
+											+ 100000) * 4095 / 200000);
+								}
+							} else {
+								//Envio el buffer 2 al dac
+								DAC_Wrapper_Set_Data_Array(&buffer_2,
+										frameData.sampleCount);
+								DAC_Wrapper_Set_Next_Buffer(&buffer_1);
+
+								//Cargo el y normalizo el buffer 1
+								res = MP3GetDecodedFrame(buffer_1,
+								MP3_DECODED_BUFFER_SIZE, &sampleCount, 0);
+								uint16_t j;
+								for (j = 0; j < frameData.sampleCount; j++) {
+									buffer_1[j] = (uint16_t) ((buffer_1[j]
+											+ 100000) * 4095 / 200000);
+								}
+							}
+
+							using_buffer_1 = !using_buffer_1;//Cambio el buffer al siguiente
+
+							i++;
+						}
+
+#ifdef DEBUG_PRINTF_INFO
+						printf("[APP] Wrote %d bytes to wav.\n", wrote);
+
+						printf("[APP] Frame %d decoded.\n", i);
+#endif
+
+#ifdef DEBUG_PRINTF_INFO
+						sr = frameData.sampleRate;
+
+						printf("[APP] FRAME SAMPLE RATE: %d \n", sr);
+#endif
+
+#ifdef DEBUG_FRAME_DELAY
+						printf("[APP] Un pequenio delay para el dios de la SD bro.\n");
+						while(!nextFrameFlag){
+							//wait
+						}
+						nextFrameFlag = false;
+#endif
+
+					} else if (res == MP3DECODER_FILE_END) {
+#ifdef DEBUG_PRINTF_APP
+							printf("[APP] FILE ENDED. Decoded %d frames.\n", i - 1);
+#endif
+						finished = true;
+						break;
+					} else {
+						int huevo = 0;
+						huevo++;
+					}
+				}
+			}
+		}
+	}
+	close_file_wav();
+//REMEMBER TO CLOSE FILES
+	printf("\nHasta la proxima\n");
+	return 0;
+}
+
+char* concat(const char *s1, const char *s2) {
+	char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
+	// in real code you would check for errors in malloc here
+	strcpy(result, s1);
+	strcat(result, s2);
+	return result;
+}
