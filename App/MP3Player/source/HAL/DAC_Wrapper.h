f8d103efc9b33d8843833172e3c7a8c1203db76e
tlondero <tlondero@itba.edu.ar>
2021-02-19 13:02:50 -0300

    Update DAC_Wrapper.h

--- App/MP3Player/source/HAL/DAC_Wrapper.h
+++ App/MP3Player/source/HAL/DAC_Wrapper.h
@@ -54,34 +54,99 @@ typedef enum {
  * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
  ******************************************************************************/
 
+/**
+ * @brief Initializes the modules: DAC, EDMA, DMA and PDB
+ */
 void DAC_Wrapper_Init(void);
 
+/**
+ * @brief Sets array to send to DAC
+ * @param
+ * 		@newDataArray -- New data
+ * 		@newSizeOf -- Size of data array
+*/
 void DAC_Wrapper_Set_Data_Array(void *newDataArray, uint32_t newSizeOf);
 
+/**
+ * @brief Set next buffer to send to DAC in order to avoid delays
+ * @param
+ * 		@forcedBackUp -- Next buffer array
+ */
 void DAC_Wrapper_Set_Next_Buffer(void *forcedBackUp);
 
+/**
+ * @brief Sets only zeros to DAC
+*/
 void DAC_Wrapper_Clear_Data_Array(void);
 
+/**
+ * @brief Initializes PDB trigger for DAC
+*/
 void DAC_Wrapper_Start_Trigger(void);		//Start trigger signal to DAC
 
+/**
+ * @brief Determinates if the it should be send the same buffer to DAC after sending all data
+*/
 void DAC_Wrapper_Loop(bool status);
 
+/**
+ * @brief Sets PDB parameters to modify DAC Fs
+ * @param
+ * 		@mod_val -- Modulus
+ * 		@mult_fact -- Multiplication factor
+ * 		@prescaler -- Prescaler
+*/
 void DAC_Wrapper_PDB_Config(uint32_t mod_val,
 		pdb_divider_multiplication_factor_t mult_fact,
 		pdb_prescaler_divider_t prescaler);
 
+/**
+ * @brief Sets DAC sample rate for standar mp3 calues
+ * @param
+ * 		@sr -- Sample rate
+ * 		@ch -- Channel: MONO / STEREO
+ *
+ * @return true if the the settings are from standar mp3
+*/
 bool MP3_Set_Sample_Rate(uint16_t sr, uint8_t ch);
 
+/**
+ * @brief Modulates signal from mp3 data to DAC
+ * @param
+ * 		@src -- source buffer
+ * 		@dst -- destiny buffer
+ *		@cnt -- size of buffer
+ *		@volume -- volume
+ *
+ * @return true if it was succesfull the initialization.
+*/
 void MP3_Adapt_Signal(int16_t *src, uint16_t *dst, uint16_t cnt, uint8_t volumen);
 
+/**
+ * @brief Check if the buffer has already send all data from buffer
+ *
+ * @return true if it ended all data
+*/
 bool DAC_Wrapper_Is_Transfer_Done(void);
 
+/**
+ * @brief Clears the transfer done flag
+*/
 void DAC_Wrapper_Clear_Transfer_Done(void);
 
+/**
+ * @brief Clears the next buffer
+*/
 void DAC_Wrapper_Clear_Next_Buffer(void);
 
+/**
+ * @brief Stops DAC
+*/
 void DAC_Wrapper_Sleep(void);
 
+/**
+ * @brief Starts DAC
+*/
 void DAC_Wrapper_Wake_Up(void);
 
 #endif /* DAC_WRAPER_H_ */
